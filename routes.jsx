edafd1bd2d725d0a7c0b20d3bbd9da6607ba7e8a
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-02-26 11:07:06 +0200

    Modify to new example of problem with statics

--- routes.jsx
+++ routes.jsx
@@ -1,31 +1,51 @@
 var React = require('react');
 var Router = require('react-router');
 var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
 var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
 
 // Components
 var App = React.createClass({
     render: function render() {
-        return <h1>App</h1>;
+        return <RouteHandler params={this.props.params} query={this.props.query} />;
+    },
+    statics: {
+        a: function() {
+            console.log('test')
+        }
     }
 });
 
 var Login = React.createClass({
     render: function render() {
         return <h1>Login</h1>;
+    },
+    statics: {
+        a: function(){
+            console.log('test');
+        }
     }
 });
 
 var NotFound = React.createClass({
     render: function render() {
         return <h1>Not found</h1>;
+    },
+    statics: {
+        a: function(){
+            console.log('test');
+        }
     }
 });
 
+// This works
+NotFound.a();
+
 var routes = (
-  <Route name="index" path="/" handler={App}>
-    <Route name="login" path="login" handler={Login}/>
-    <NotFoundRoute handler={NotFound}/>
+  <Route path="/" handler={App}>
+    <DefaultRoute name="index" handler={Login} />
+    <NotFoundRoute handler={NotFound} />
   </Route>
 );
 
--- run.js
+++ run.js
@@ -3,13 +3,13 @@ var React = require('react');
 var ReactRouter = require('react-router');
 
 var routes = require('./routes.jsx');
-
 ReactRouter.run(routes, '/login', function(Handler, state) {
+    // This does not work
+    Handler.a();
+
     var handler = React.createElement(Handler, {
         params: state.params,
         query: state.query
     });
     var initialHtml = React.renderToString(handler);
-
-    console.log(initialHtml);
 });
